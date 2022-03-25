91e4e26338c61d85ef1475e9fa9c02a5b6db5598
Anup Narvekar <narvekaranup321@gmail.com>
2022-03-25 23:56:04 +0530

    added form data handler

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,18 +3,18 @@ const ContactForm = () => {
         <div className="contactForm">
             <p className="title">Conatct Form</p>
             <p>Fill out this form and we'll get in touch with you soon</p>
-            <form action="">
+            <form action="https://formspree.io/f/meqnloaa">
                 <label htmlFor="fname"></label>
-                <input type="text" id='fname' placeholder='First Name' required />
+                <input type="text" id='fname' placeholder='First Name' name="fname" required />
 
                 <label htmlFor="lname"></label>
-                <input type="text" id='lname' placeholder='Last Name' required />
+                <input type="text" id='lname' placeholder='Last Name' name="lname" required />
 
                 <label htmlFor="phone"></label>
-                <input type="tel" id='phone' placeholder='Phone' required />
+                <input type="tel" id='phone' placeholder='Phone' name="phone" required />
 
                 <label htmlFor="email"></label>
-                <input type="email" id='email' placeholder='Email' />
+                <input type="email" id='email' placeholder='Email' name="email" />
 
                 <label htmlFor="message"></label>
                 <textarea name="message" id="message"  placeholder='Your Message' required></textarea>
